49c507b99c5553c08c56f62c6ba2a41ebd223d60
Matthew Maurer <matthew.r.maurer@gmail.com>
2017-04-09 03:09:14 -0400

    Also build with OCaml 4.03

--- bap.nix
+++ bap.nix
@@ -2,7 +2,9 @@
 
 rec {
   bap = nixpkgs.ocamlPackages.bap;
+  bap_4_03 = nixpkgs.ocamlPackages_4_03.bap;
   bap_python = nixpkgs.pythonPackages.bap;
   libbap = nixpkgs.libbap;
   bap_testsuite = nixpkgs.callPackage ./bap-testsuite.nix { inherit bap; };
+  bap_testsuite_4_03 = nixpkgs.callPackage ./bap-testsuite.nix { bap = bap_4_03; };
 }
